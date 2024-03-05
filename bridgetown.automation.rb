ac951eaa975019a249f2e74094fe1ee127f8b739
Syed M. Sawaid <34231494+SyedMSawaid@users.noreply.github.com>
2024-03-05 20:53:10 +0500

    Initial Commit

---
+++ bridgetown.automation.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+say_status :design, "Installing Tailwind CSS..."
+
+confirm = ask "This configuration will remove all the unnecessary files. Do you want to continue? [Yn]"
+return unless confirm.casecmp?("Y")
+
+# Delete folders
+run "rm -rf ./src/_posts"
+
+# Delete files
+run "rm ./src/about.md"
+run "rm ./src/index.md"
+run "rm ./src/posts.md"
+run "rm ./src/layouts/post.liquid"
+
+run "bin/bridgetown apply https://github.com/bridgetownrb/tailwindcss-automation"
+
+say_status :design, "Bridgetown has been configured for designing."
